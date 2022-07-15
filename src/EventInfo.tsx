d50e0bdbc5133ec22bb25473a96aed6dad7a25a9
Martijn Faassen <faassen@startifact.com>
2022-07-15 15:47:14 +0200

    Rewrite to use flexbox as a table is overkill

--- src/EventInfo.tsx
+++ src/EventInfo.tsx
@@ -5,12 +5,21 @@ import { DeltaChatEvent } from "./event";
 
 const InfoRow: Component<{ label: string; value: any }> = (props) => {
   return (
-    <tr>
-      <td class="w-1/12 font-semibold">{props.label}</td>
-      <td>
-        <p>{props.value}</p>
-      </td>
-    </tr>
+    <div class="flex">
+      <div class="w-1/6 font-semibold">{props.label}</div>
+      <div>{props.value}</div>
+    </div>
+  );
+};
+
+const EventInfoContent: Component<{ value: DeltaChatEvent }> = (props) => {
+  return (
+    <div class="m-6 flex flex-col">
+      <InfoRow label="ts" value={props.value.ts} />
+      <InfoRow label="event" value={props.value.event_type} />
+      <InfoRow label="data1" value={props.value.data1} />
+      <InfoRow label="data2" value={props.value.data2} />
+    </div>
   );
 };
 
@@ -23,14 +32,7 @@ const EventInfo: Component<{
     <Show when={props.value()}>
       {(value) => (
         <Modal isOpen={props.isOpen} onClose={props.onClose}>
-          <table class="my-14 ml-10 w-full border">
-            <tbody>
-              <InfoRow label="ts" value={value.ts} />
-              <InfoRow label="event" value={value.event_type} />
-              <InfoRow label="data1" value={value.data1} />
-              <InfoRow label="data2" value={value.data2} />
-            </tbody>
-          </table>
+          <EventInfoContent value={value} />
         </Modal>
       )}
     </Show>
