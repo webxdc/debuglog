4af7cfbed77ce8dbab5feef68e2d3b912c4694fe
Martijn Faassen <faassen@startifact.com>
2022-07-15 17:45:43 +0200

    Bigger text for buttons

--- src/Button.tsx
+++ src/Button.tsx
@@ -7,7 +7,7 @@ const Button: Component<JSX.IntrinsicElements["button"]> = (props) => {
   hover:text-blue-700 focus:z-10 focus:outline-none focus:ring-4
   focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800
   dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white
-  dark:focus:ring-gray-700`;
+  dark:focus:ring-gray-700 text-sm md:text-base lg:text-lg xl:2xl:text-2xl`;
   props = mergeProps(props, { class: mergeClass(props.class, twClasses) });
   return <button {...props} />;
 };
