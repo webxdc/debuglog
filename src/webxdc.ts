629964e243ff70ca392a8d6e168a3502115559bc
Martijn Faassen <faassen@startifact.com>
2022-07-19 13:31:57 +0200

    Use an id counter instead of relying on serial, so we can guarantee it always starts at 0.

--- src/webxdc.ts
+++ src/webxdc.ts
@@ -1,7 +1,10 @@
 import { addEvent } from "./store";
 
+let idCounter = 0;
+
 export function installUpdateListener() {
   window.webxdc.setUpdateListener((update) => {
-    addEvent({ ...update.payload, id: update.serial });
+    addEvent({ ...update.payload, id: idCounter });
+    idCounter++;
   }, 0);
 }
